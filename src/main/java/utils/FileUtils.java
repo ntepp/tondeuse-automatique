32d6c2d047971abc7e61c547f38f66ca0f661727
ntepp <ntepp.marcus@gmail.com>
2021-09-30 02:37:56 +0400

    implement Testutils class

--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -1,6 +1,9 @@
 package utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,8 +12,15 @@ public class FileUtils {
     public static final String INPUT_FILE = "/input.text";
 
     public List<String> readFile() throws IOException {
-
-        return new ArrayList<>();
+        List<String> inputData = new ArrayList<>();
+        InputStream inputStream = getClass().getResourceAsStream(INPUT_FILE);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                inputData.add(line);
+            }
+        }
+        return inputData;
     }
 
 }
